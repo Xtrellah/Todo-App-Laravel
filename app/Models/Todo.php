7ef56e95f19406fc067ffe797ffba7cdb4a2d7a7
Max Estrella <maxestrella565@gmail.com>
2024-08-08 15:25:31 +0100

    Minor changes

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,6 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['completed'];
 }
--- resources/views/TodoList.blade.php
+++ resources/views/TodoList.blade.php
@@ -1,14 +1,17 @@
 
+<script src="https://cdn.tailwindcss.com"></script>
 <body>
 
-    <h1>{{$title}}</h1>
+    <h1 class="text-5xl m-8 underline underline-offset-8">{{$title}}</h1>
 
-    <ul>
+    <ul class="m-8">
         @foreach($todos as $todo)
-            <li>
-                <h3>{{$todo->name}}</h3>
+            <li class="border">
+                <p class="font-bold">{{$todo->name}}</p>
                 <p>{{$todo->description}}</p>
+                <p>Completed: {{$todo->completed}}</p>
             </li>
+            <br>
         @endforeach
     </ul>
 
